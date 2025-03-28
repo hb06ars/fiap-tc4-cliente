e1741417f4abbea9ac4a76bc970c58d0c84e0682
SISGDSOLAR <32486795+hb06ars@users.noreply.github.com>
2025-03-28 15:17:28 -0300

    a
    a

--- src/main/java/org/fiap/infra/exceptions/RecordAlreadyExistsException.java
+++ src/main/java/org/fiap/infra/exceptions/RecordAlreadyExistsException.java
@@ -1,7 +1,6 @@
 package org.fiap.infra.exceptions;
 
 public class RecordAlreadyExistsException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
 
     public RecordAlreadyExistsException(String message, Throwable cause) {
         super(message, cause);
